29bbb641cdcb81199c61fbef88f63e99bc03cc3a
websiteslpro <41922871+websiteslpro@users.noreply.github.com>
2018-08-30 23:40:09 +0530

    Add files via upload

---
+++ assets/js/index.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){$(".owl-carousel").owlCarousel({loop:!0,margin:30,autoplay:!0,autoplayHoverPause:!0,slideBy:3,mouseDrag:!1,dotsContainer:".owl-dots",responsive:{0:{items:1},768:{items:2},992:{items:2},1200:{items:3}}})});
+ $(".button-fill").hover(function () {
+    $(this).children(".button-inside").addClass('full');
+}, function() {
+  $(this).children(".button-inside").removeClass('full');
+});
